d195fa5cd76ac232046350cdf0552f6438bc0b29
Xavier Austin <166980912+xavierAustin@users.noreply.github.com>
2025-05-21 04:32:28 -0700

    :(

--- Assets/Scripts/UI/RelicUI.cs
+++ Assets/Scripts/UI/RelicUI.cs
@@ -4,48 +4,40 @@ using TMPro;
 
 public class RelicUI : MonoBehaviour
 {
-    //public PlayerController player;
-    //public int index;
-
-    //public Image icon;
-    //public GameObject highlight;
-    //public TextMeshProUGUI label;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    //void Start()
-    //{
-        // if a player has relics, this is how you *could* show them
-        //player = GameManager.Instance.player;
-        //Relic r = RelicSystem.Instance.activeRelics[index];
-    //}
-
-    // Update is called once per frame
-    //void Update()
-    //{
-        // Relics could have labels and/or an active-status
-        //Relic r = player.relics[index];
-        //label.text = r.GetLabel();
-        //highlight.SetActive(r.IsActive());
-    //}
+    public PlayerController player;
+    public int index;
 
     public Image icon;
     public GameObject highlight;
+    public TextMeshProUGUI label;
     public Relic relic;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //last_text_update = 0;
-    }
+        // if a player has relics, this is how you *could* show them
 
+        //Relic r = player.relics[index];
+    }
     public void SetRelic(Relic relic)
     {
         this.relic = relic;
-        icon = this.GetComponent<Image>();
-        icon.enabled = true;
-        GameManager.Instance.relicIconManager.PlaceSprite(relic.sprite, icon);
+        
+        //GameManager.Instance.relicIconManager.PlaceSprite(relic.sprite, icon);
+        //icon = this.GetComponent<Image>();
+        //icon.enabled = true;
+        //icon.sprite = GameManager.Instance.relicIconManager.Get(relic.sprite);
+        //GameManager.Instance.relicIconManager.PlaceSprite(relic.sprite, icon);
     }
 
+    // Update is called once per frame
     void Update()
     {
-
+        // Relics could have labels and/or an active-status
+        /*
+        Relic r = player.relics[index];
+        label.text = r.GetLabel();
+        highlight.SetActive(r.IsActive());
+        */
     }
 }
